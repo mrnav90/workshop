c6e55241e265f02303142a96062774bb7af46d52
VietNa <mr.nav90@gmail.com>
2016-06-15 10:36:48 +0700

    update database

--- db/mongo-workshop-20160615-053550.js
+++ db/mongo-workshop-20160615-053550.js
@@ -27,6 +27,18 @@ db.getCollection("AccessToken").insert({
   "created": ISODate("2016-06-14T03:26:54.655Z"),
   "userId": ObjectId("575f7910530962b40bbb8162")
 });
+db.getCollection("AccessToken").insert({
+  "_id": "bc8eDeTki3n5fozRp46c9KkHS8CkyNDIsspE18vSqy1neAcLjltFuYAbW7UAgP0w",
+  "ttl": NumberInt(1209600),
+  "created": ISODate("2016-06-14T08:41:55.663Z"),
+  "userId": ObjectId("575f7910530962b40bbb8162")
+});
+db.getCollection("AccessToken").insert({
+  "_id": "eAOES7Kd7MrlZ1YkITG4fHl1pDIIA30YMSFyIJEtjtyClQSm9tBacYHNXODqXs4v",
+  "ttl": NumberInt(1209600),
+  "created": ISODate("2016-06-14T09:29:36.704Z"),
+  "userId": ObjectId("575f7910530962b40bbb8162")
+});
 
 /** Product records **/
 db.getCollection("Product").insert({
@@ -44,5 +56,6 @@ db.getCollection("User").insert({
   "password": "$2a$10$NkynY2IvCbfKZQpDCbK5ZeC6Pt8B7onBsk.9H4pi0KT3YrduZV38a",
   "email": "mr.nav90@gmail.com",
   "created": ISODate("2016-06-14T00:00:00.0Z"),
-  "displayname": "Ngo Anh Viet"
+  "displayname": "Ngo Anh Viet",
+  "avatar": "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.0-9/13344748_1730264523858606_3240140766714948266_n.jpg?oh=bc9383baf0b1369099ed699bc3eb4595&oe=580AEB11"
 });
